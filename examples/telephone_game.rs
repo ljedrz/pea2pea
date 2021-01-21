bd882e05b9a5def7285b2c6f8338d202c23d1e7f
ljedrz <ljedrz@gmail.com>
2021-01-21 15:51:40 +0100

    examples: improve the telephone game
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- examples/telephone_game.rs
+++ examples/telephone_game.rs
@@ -20,6 +20,8 @@ impl Pea2Pea for Player {
     }
 }
 
+const NUM_PLAYERS: usize = 100;
+
 #[async_trait::async_trait]
 impl Reading for Player {
     type Message = String;
@@ -27,7 +29,9 @@ impl Reading for Player {
     fn read_message(&self, _src: SocketAddr, buffer: &[u8]) -> io::Result<Option<(String, usize)>> {
         if buffer.len() >= 2 {
             let payload_len = u16::from_le_bytes(buffer[..2].try_into().unwrap()) as usize;
-            if payload_len == 0 { return Err(io::ErrorKind::InvalidData.into()); }
+            if payload_len == 0 {
+                return Err(io::ErrorKind::InvalidData.into());
+            }
 
             if buffer[2..].len() >= payload_len {
                 let message = String::from_utf8(buffer[2..][..payload_len].to_vec())
@@ -43,12 +47,14 @@ impl Reading for Player {
     }
 
     async fn process_message(&self, source: SocketAddr, message: String) -> io::Result<()> {
+        let own_id = self.node().name().parse::<usize>().unwrap();
+
         info!(
             parent: self.node().span(),
-            "{} said \"{}\"{}",
-            source,
+            "player {} said \"{}\"{}",
+            own_id - 1,
             message,
-            if self.node().name() != "99" { ", passing it on" } else { "" },
+            if own_id != NUM_PLAYERS - 1 { ", passing it on" } else { "" },
         );
 
         let connected_addrs = self.node().connected_addrs();
@@ -88,23 +94,28 @@ async fn main() {
         .with_target(false)
         .init();
 
-    let mut players = Vec::with_capacity(100);
-    for _ in 0..100 {
+    let mut players = Vec::with_capacity(NUM_PLAYERS);
+    for _ in 0..NUM_PLAYERS {
         let player = Player(Node::new(None).await.unwrap());
         players.push(player);
     }
 
+    // technically the first node doesn't need `Reading` and the last one doesn't need `Writing`
     for player in &players {
         player.enable_reading();
         player.enable_writing();
     }
     connect_nodes(&players, Topology::Line).await.unwrap();
 
-    let message = b"when we can't think for ourselves, we can always quote";
+    let message = "when we can't think for ourselves, we can always quote";
 
+    info!(parent: players[0].node().span(), "psst, player {}; \"{}\", pass it on!", players[1].node().name(), message);
     players[0]
         .node()
-        .send_direct_message(players[1].node().listening_addr(), message[..].into())
+        .send_direct_message(
+            players[1].node().listening_addr(),
+            message.as_bytes().into(),
+        )
         .await
         .unwrap();
 
