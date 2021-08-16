4b56c004f46ef7099f748424920706e2745e0282
ljedrz <ljedrz@gmail.com>
2021-08-16 11:10:50 +0200

    tests: improve the fuzz test
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- tests/fuzzing.rs
+++ tests/fuzzing.rs
@@ -1,4 +1,5 @@
 use rand::{distributions::Standard, rngs::SmallRng, Rng, SeedableRng};
+use tokio::time::sleep;
 
 mod common;
 use pea2pea::{
@@ -6,41 +7,65 @@ use pea2pea::{
     Config, Node, Pea2Pea,
 };
 
+use std::time::Duration;
+
 #[tokio::test(flavor = "multi_thread")]
 async fn fuzzing() {
-    const MAX_MSG_SIZE: usize = 1024 * 1024;
+    // tracing_subscriber::fmt::init();
+
+    const MAX_MSG_SIZE: usize = 4096;
+    const NUM_MESSAGES: usize = 1_000;
 
     let config = Config {
+        name: Some("receiver".into()),
         read_buffer_size: MAX_MSG_SIZE,
         ..Default::default()
     };
-    let tester = common::MessagingNode(Node::new(Some(config)).await.unwrap());
-    tester.enable_reading();
+    let receiver = common::MessagingNode(Node::new(Some(config)).await.unwrap());
+    receiver.enable_reading();
+    let receiver_addr = receiver.node().listening_addr().unwrap();
 
-    let sender = common::MessagingNode(Node::new(None).await.unwrap());
+    let config = Config {
+        name: Some("sender".into()),
+        ..Default::default()
+    };
+    let sender = common::MessagingNode(Node::new(Some(config)).await.unwrap());
     sender.enable_writing();
 
-    sender
-        .node()
-        .connect(tester.node().listening_addr().unwrap())
-        .await
-        .unwrap();
+    sender.node().connect(receiver_addr).await.unwrap();
 
-    wait_until!(1, tester.node().num_connected() == 1);
+    wait_until!(1, receiver.node().num_connected() == 1);
 
     let mut rng = SmallRng::from_entropy();
 
-    for _ in 0..1_000 {
+    let mut expected_msg_count: u64 = 0;
+    let mut expected_msg_size: u64 = 0;
+    let mut processed_sizes: Vec<usize> = Vec::with_capacity(NUM_MESSAGES);
+
+    for _ in 0..NUM_MESSAGES {
         let random_len: usize = rng.gen_range(1..=MAX_MSG_SIZE - 4); // account for the length prefix
         let random_payload: Vec<u8> = (&mut rng).sample_iter(Standard).take(random_len).collect();
-        // ignore full outbound queue channel errors
-        let _ = sender.node().send_direct_message(
-            tester.node().listening_addr().unwrap(),
-            random_payload.into(),
-        );
 
-        if tester.node().num_connected() == 0 {
-            panic!("the fuzz test failed!");
+        // skip full channel errors
+        if sender
+            .node()
+            .send_direct_message(receiver_addr, random_payload.into())
+            .is_ok()
+        {
+            processed_sizes.push(random_len);
+            expected_msg_count += 1;
+            expected_msg_size += 4 + random_len as u64;
+        }
+
+        sleep(Duration::from_millis(5)).await;
+
+        if receiver.node().num_connected() == 0 {
+            panic!("the fuzz test failed! message sizes: {:?}", processed_sizes);
         }
     }
+
+    wait_until!(
+        1,
+        receiver.node().stats().received() == (expected_msg_count, expected_msg_size)
+    );
 }
