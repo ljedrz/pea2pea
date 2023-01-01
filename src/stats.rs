bc910e5a6e9de4457bb7781ad4da5841f3a4a366
ljedrz <ljedrz@gmail.com>
2023-01-01 22:02:00 +0100

    docs: add extra notes to the Stats
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- src/stats.rs
+++ src/stats.rs
@@ -3,6 +3,9 @@ use std::{
     time::Instant,
 };
 
+#[cfg(doc)]
+use crate::protocols::{Handshake, Reading, Writing};
+
 /// Contains basic statistics related to a node or a connection.
 pub struct Stats {
     /// The creation time.
@@ -31,23 +34,34 @@ impl Default for Stats {
 
 impl Stats {
     /// Registers a sent message of the provided `size` in bytes.
+    ///
+    /// note: It's triggered automatically for the node and every connection
+    /// when using the [`Writing`] protocol; it is public in case the user
+    /// also wants to collect stats during [`Handshake::perform_handshake`].
     pub fn register_sent_message(&self, size: usize) {
         self.msgs_sent.fetch_add(1, Relaxed);
         self.bytes_sent.fetch_add(size as u64, Relaxed);
     }
 
     /// Registers a received message of the provided `size` in bytes.
+    ///
+    /// note: It's triggered automatically for the node and every connection
+    /// when using the [`Reading`] protocol; it is public in case the user
+    /// also wants to collect stats during [`Handshake::perform_handshake`].
     pub fn register_received_message(&self, size: usize) {
         self.msgs_received.fetch_add(1, Relaxed);
         self.bytes_received.fetch_add(size as u64, Relaxed);
     }
 
-    /// Returns the creation time.
+    /// Returns the creation time of the node or a connection.
     pub fn created(&self) -> Instant {
         self.created
     }
 
     /// Returns the number of sent messages and their collective size in bytes.
+    ///
+    /// note: Connection stats indicate the number of messages sent from the node
+    /// to the corresponding connection.
     pub fn sent(&self) -> (u64, u64) {
         let msgs = self.msgs_sent.load(Relaxed);
         let bytes = self.bytes_sent.load(Relaxed);
@@ -56,6 +70,9 @@ impl Stats {
     }
 
     /// Returns the number of received messages and their collective size in bytes.
+    ///
+    /// note: Connection stats indicate the number of messages received by the node
+    /// from the corresponding connection.
     pub fn received(&self) -> (u64, u64) {
         let msgs = self.msgs_received.load(Relaxed);
         let bytes = self.bytes_received.load(Relaxed);
